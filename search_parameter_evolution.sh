72da45bf36356bada98cc5b37de6134dda207677
toyama@gavo <toyama@gavo.t.u-tokyo.ac.jp>
2016-02-16 22:33:34 +0900

    search all weapon parameters

--- search_parameter_evolution.sh
+++ search_parameter_evolution.sh
@@ -10,11 +10,13 @@ settingInit=$setting.init
 initParam="1 1 1 1 1 1 1 1 1"
 weapon=0
 amplitude=1.0
-ratio=0.85
-childNum=10
-matchCount=10
-maxIter=100
-minAmplitude=0.2
+ratio=0.90
+childNum=5
+matchCount=20
+maxIter=50
+minAmplitude=0.9
+
+mkdir -p setting log
 
 function game() {
 	manager/gameManager \
@@ -39,54 +41,63 @@ function winner2result() {
 	fi
 }
 
-mkdir -p setting log
-
-rm -f $setting
-if [ -e $setting ]; then
-	echo "read $setting line 1-3 as initial state"
-else
-	echo "use '$initParam' as initial state"
-	for k in $(seq 3); do
-		echo $initParam >> $setting
-	done
-fi
-cp $setting $settingInit
-
-if [ $# -eq 1 ]; then
-	weapon=$1
-fi
-echo "search weapon[$weapon]"
-
-echo "parameter search"
-echo "initial parameter: $settingInit, child number:$childNum, match count: $matchCount"
-rm -f $result
-touch $result
-for iter in $(seq $maxIter); do
-	echo "iteration: $iter/$maxIter ( amplitude: $amplitude )"
-	python generateSetting.py $settingAll $settingInit $result $childNum $matchCount $weapon $amplitude $minAmplitude
-	if [ $(wc -l $settingAll | cut -d " " -f 1) -eq 6 ]; then # end search
-		break
+function search_param() {
+	#rm -f $setting
+	if [ -e $setting ]; then
+		echo "read $setting line 1-3 as initial state"
+	else
+		echo "use '$initParam' as initial state"
+		for k in $(seq 3); do
+			echo $initParam >> $setting
+		done
 	fi
+	cp $setting $settingInit
 
-	for child in $(seq $(($childNum*2))); do # sente / gote
-		head -n $(($child*6)) $settingAll | tail -n 6 > $setting
-		for k in $(seq $matchCount); do
-			echo -e "$child/$(($childNum*2)) $k/$matchCount\r\c"
-			begin_line=$(wc $result -l)
-			game > res
-			head -n 2 res | tail -n 1 | cut -d "[" -f 2 | cut -d "]" -f 1 | sed -e "s/ //g" -e "s/,/\n/g" > scores
-			winner2result $(head -n 4 res | tail -n 1 | cut -d '"' -f 4) | sed -e "s/ /\n/g" > winner
-			paste -d " " $setting scores winner >> $result
-			while [ "$begin_line" = "$(wc $result -l)" ]; do
-				sleep 0.05s
+	if [ $# -eq 1 ]; then
+		weapon=$1
+	fi
+	echo "search weapon[$weapon]"
+
+	echo "initial parameter: $settingInit, child number:$childNum, match count: $matchCount"
+	rm -f $result
+	touch $result
+	for iter in $(seq $maxIter); do
+		echo "iteration: $iter/$maxIter ( amplitude: $amplitude )"
+		python generateSetting.py $settingAll $settingInit $result $childNum $matchCount $weapon $amplitude $minAmplitude
+		if [ $(wc -l $settingAll | cut -d " " -f 1) -eq 6 ]; then # end search
+			break
+		fi
+
+		for child in $(seq $(($childNum*2))); do # sente / gote
+			head -n $(($child*6)) $settingAll | tail -n 6 > $setting
+			for k in $(seq $matchCount); do
+				echo -e "$child/$(($childNum*2)) $k/$matchCount\r\c"
+				begin_line=$(wc $result -l)
+				game > res
+				head -n 2 res | tail -n 1 | cut -d "[" -f 2 | cut -d "]" -f 1 | sed -e "s/ //g" -e "s/,/\n/g" > scores
+				winner2result $(head -n 4 res | tail -n 1 | cut -d '"' -f 4) | sed -e "s/ /\n/g" > winner
+				paste -d " " $setting scores winner >> $result
+				while [ "$begin_line" = "$(wc $result -l)" ]; do
+					sleep 0.05s
+				done
 			done
 		done
+		amplitude=$( echo "scale=4;$amplitude*$ratio" | bc )
 	done
-	amplitude=$( echo "scale=4;$amplitude*$ratio" | bc )
+
+	echo "training end"
+	cat $settingAll > $setting
+
+	# remove tmp files
+	rm -f winner scores $settingInit $settingAll SamuraiLog*
+}
+
+beginAmplitude=$amplitude
+for i in $(seq 10); do
+	echo "=== epoch $i ==="
+	for weapon in $(seq 0 2); do
+		amplitude=$beginAmplitude
+		search_param $weapon
+	done
+	beginAmplitude=$( echo "scale=4;$beginAmplitude*$ratio" | bc )
 done
-
-echo "training end"
-cat $settingAll > $setting
-
-# remove tmp files
-rm -f winner scores $settingInit $settingAll SamuraiLog*
